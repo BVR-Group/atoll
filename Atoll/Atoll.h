cc945d11f97f52a6b89eb8ea8b6988110fa1e5a3
Dylan Wreggelsworth <dylan@bvr.io>
2017-06-12 21:18:51 -0400

    Project setup

---
+++ Atoll/Atoll.h
@@ -0,0 +1,19 @@
+//
+//  Atoll.h
+//  Atoll
+//
+//  Created by Dylan Wreggelsworth on 6/12/17.
+//  Copyright © 2017 BVR, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Atoll.
+FOUNDATION_EXPORT double AtollVersionNumber;
+
+//! Project version string for Atoll.
+FOUNDATION_EXPORT const unsigned char AtollVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Atoll/PublicHeader.h>
+
+
